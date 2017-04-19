1f84f42e5852da177a5df94fd04b3a4e289e17d4
Chitroopa Manikkavasagam and Dan Jacob <chitroopa@>
2017-04-19 16:31:26 -0700

    fix bug

--- spec/rock_paper_scissor_integration_spec.rb
+++ spec/rock_paper_scissor_integration_spec.rb
@@ -6,8 +6,8 @@ set(:show_exceptions, false)
 describe('rock_paper_scissor', {:type => :feature}) do
   it('accepts user input and displays result') do
     visit('/')
-    choose('player1', checked: 'Rock')
-    choose('player2', checked: 'Paper')
+    choose('player1rock', checked: 'Rock')
+    choose('player2paper', checked: 'Paper')
     click_button('Play')
     expect(page).to have_content('Player2 wins')
   end
